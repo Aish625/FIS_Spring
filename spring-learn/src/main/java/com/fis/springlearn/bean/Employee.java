35fa1b391a8b8b13912afc8f17884d4edd69f728
Aish625 <aishwaraya625@gmail.com>
2021-08-10 15:54:17 +0530

    added multiple beans

--- spring-learn/src/main/java/com/fis/springlearn/bean/Employee.java
+++ spring-learn/src/main/java/com/fis/springlearn/bean/Employee.java
@@ -1,5 +1,6 @@
 package com.fis.springlearn.bean;
 
+import java.util.Arrays;
 import java.util.Date;
 
 import org.slf4j.Logger;
@@ -14,6 +15,8 @@ public class Employee {
 	double	salary;
 	boolean	permanent;
 	Date	dateOfBirth;
+	Department department;
+	Skill[] skills;
 	public Employee()
 	{
 		LOGGER.debug("Inside Employee Constructor");
@@ -42,6 +45,18 @@ public class Employee {
 	public void setPermanent(boolean permanent) {
 		this.permanent = permanent;
 	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public Skill[] getSkills() {
+		return skills;
+	}
+	public void setSkills(Skill[] skills) {
+		this.skills = skills;
+	}
 	public Date getDateOfBirth() {
 		return dateOfBirth;
 	}
@@ -51,7 +66,11 @@ public class Employee {
 	@Override
 	public String toString() {
 		return "Employee [id=" + id + ", name=" + name + ", salary=" + salary + ", permanent=" + permanent
-				+ ", dateOfBirth=" + dateOfBirth + "]";
+				+ ", dateOfBirth=" + dateOfBirth + ", department=" + department + ", skills=" + Arrays.toString(skills)
+				+ ", getId()=" + getId() + ", getName()=" + getName() + ", getSalary()=" + getSalary()
+				+ ", isPermanent()=" + isPermanent() + ", getDepartment()=" + getDepartment() + ", getSkills()="
+				+ Arrays.toString(getSkills()) + ", getDateOfBirth()=" + getDateOfBirth() + ", getClass()=" + getClass()
+				+ ", hashCode()=" + hashCode() + ", toString()=" + super.toString() + "]";
 	}
 	
 	
